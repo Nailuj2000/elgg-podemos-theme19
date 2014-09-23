aa07c852133b96095e5582b097bfc75a06d3803b
nestor <eslivion@gmail.com>
2014-09-23 19:55:35 +0200

    Colocado el buscador a la derecha del topbar

--- views/default/page/elements/topbar.php
+++ views/default/page/elements/topbar.php
@@ -9,12 +9,14 @@
 echo elgg_view('navigation/breadcrumbs');
 
 // Elgg logo
+
+//INCLUIMOS LA VISTA DEL BUSCADOR EN LA TOPBAR
+//Todo: averiguar por qué poniéndola antes, aparece después de la vista del menú topbar
+echo elgg_view('search/header',$vars);
+
 echo elgg_view_menu('topbar', array('sort_by' => 'priority', array('elgg-menu-hz')));
 
 // elgg tools menu
 // need to echo this empty view for backward compatibility.
 echo elgg_view_deprecated("navigation/topbar_tools", array(), "Extend the topbar menus or the page/elements/topbar view directly", 1.8);
 
-// INCLUIMOS EL BUSCADOR EN LA VISTA TOPBAR
-//Todo: que se muestre a la derecha de los otros elementos (quizás sea la prioridad)
-echo elgg_view('search/header',$vars);
