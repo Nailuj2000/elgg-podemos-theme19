ec107520d879dabf94179a47f3f6531b6476de9d
jonabasque <jonmultimedia@gmail.com>
2014-10-29 18:29:25 +0100

    Añadido link a los breadcrums

--- views/default/navigation/breadcrumbs.php
+++ views/default/navigation/breadcrumbs.php
@@ -11,7 +11,7 @@
  * @see elgg_push_breadcrumb
  */
 
-// SI EXISTEN BREADCRUMBS EN $vars LAS RECOGEMOS Y SI NO LAS RECOGEMOS 
+// SI EXISTEN BREADCRUMBS EN $vars LAS RECOGEMOS Y SI NO LAS RECOGEMOS
 if (isset($vars['breadcrumbs'])) {
 	$breadcrumbs = $vars['breadcrumbs'];
 } else {
@@ -20,7 +20,7 @@ if (isset($vars['breadcrumbs'])) {
 /*
 foreach ($breadcrumbs as $bread){
 	print_r($bread);
-	echo "<br />";	
+	echo "<br />";
 } */
 $class = 'elgg-menu elgg-breadcrumbs';
 $additional_class = elgg_extract('class', $vars, '');
@@ -40,7 +40,7 @@ if(count($breadcrumbs)>0){
 }
 
 //$image2="flecha.png";
-	
+
 //FUNCIONA. SE RECOGE LA URL DEL SITIO, NO SU RUTA DENTRO DEL SERVIDOR
 $path_image= elgg_get_site_url()."mod/podemos_theme19/graphics/".$image;
 //$path_image2= elgg_get_site_url()."mod/podemos_theme19/graphics/".$image2;
@@ -64,18 +64,19 @@ if (is_array($breadcrumbs) && count($breadcrumbs) > 0) {
 
 	$number_breadcrumbs = count($breadcrumbs);
 	for ($i = 1;$i<$number_breadcrumbs; $i++) {
-
-		/*if (!empty($breadcrumbs[$i]['link'])) {
+		/*
+		if (!empty($breadcrumbs[$i]['link'])) {
 			$breadcrumb = elgg_view('output/url', array(
 			));
 		} else {
 			$breadcrumb = $breadcrumbs[$i]['title'];
-		}*/
-		
+		}
+		*/
 		//SE VAN A MOSTRAR TODAS Y CADA UNA DE LAS BREADCRUMBS
 		$breadcrumb = $breadcrumbs[$i]['title'];
+		$breadcrumb_link = $breadcrumbs[$i]['link'];
 
-		echo "<li><div class='flecha_reves'></div><div class='contenido'>$breadcrumb</div><div class='flecha'></div></li>";
+		echo "<li><div class='flecha_reves'></div><div class='contenido'><a href='$breadcrumb_link'>$breadcrumb</a></div><div class='flecha'></div></li>";
 	}
 }
 
